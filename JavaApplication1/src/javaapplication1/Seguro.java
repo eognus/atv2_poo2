f6bc839d55ee1c7e0ac74654af8180bd99b2f39b
eognus <103257622+eognus@users.noreply.github.com>
2023-08-07 08:05:39 -0300

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/Seguro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Seguro {
+    int valor;
+    boolean vigencia;
+    Cliente proprietario;
+    ArrayList<Cliente> assegurados = new ArrayList<Cliente>();
+}
---
+++ JavaApplication1/src/javaapplication1/Veículo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Veículo {
+    int ano;
+    String marca;
+    String cor;
+    String placa;
+    Modelo modelo;
+}
